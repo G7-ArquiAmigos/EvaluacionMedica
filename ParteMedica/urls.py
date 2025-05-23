9e48bb1f1cc25d0e899adb4a2befc1cf895da044
Tdiazv2 <t.diazv@uniandes.edu.co>
2025-05-23 18:40:51 -0500

    interfaz evaluacion

--- ParteMedica/urls.py
+++ ParteMedica/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.decorators.csrf import csrf_exempt
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',views.home),
+    path('evaluacionMedica/',include('DiagnosticoMedico.urls')),
+    path('', views.index),
 ]
