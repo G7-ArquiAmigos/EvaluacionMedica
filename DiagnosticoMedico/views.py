bd9d0f8005b55f7eec7e7fc1171cb6e77b682071
Tdiazv2 <123430733+Tdiazv2@users.noreply.github.com>
2025-05-28 18:51:41 -0500

    Update views.py

--- DiagnosticoMedico/views.py
+++ DiagnosticoMedico/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Diagnóstico Médico, PUTO LUCAS")
+    return HttpResponse("Diagnóstico Médico")
