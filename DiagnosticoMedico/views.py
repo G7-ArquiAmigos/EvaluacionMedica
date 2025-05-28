70eef374e2334bcacd1bd75521f211c2cb283f60
Tdiazv2 <123430733+Tdiazv2@users.noreply.github.com>
2025-05-28 18:53:31 -0500

    Update views.py

--- DiagnosticoMedico/views.py
+++ DiagnosticoMedico/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(<head>
-  <meta charset="UTF-8">
-  <title>Hola Mundo</title>
-</head>
-<body>
-  <h1>Hola Mundo</h1>
-</body>)
+    return HttpResponse("En este espacio son las citas medicas" )
